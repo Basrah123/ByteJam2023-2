cf6a73de86b733c4ea76dc401b26e039b2cff7d2
LuisJl22 <lsjl22@outlook.com>
2023-11-09 09:56:04 -0600

    what we did

---
+++ Assets/Key.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+
+/** These are the functions for the bullets from the keyboard 
+  which in this are the 'keys' from the keyboard 
+*/
+ 
+  // const variables 
+   public float life = 3;
+    // Find functions of these constructors/method 
+
+    void Awake()
+    {
+        Destroy(gameObject, life); 
+    }
+
+    void OnCollisionEnter(Collision collision) 
+    {
+        Destroy(collision.gameObject); 
+        Destroy(gameObject);         
+    }
+
+    /* allow for the bullets to not destroy the ground/floor/walls/levels or any other items 
+    of high importance, might be better to simplify the on collision method to only allow the bullets or 'keys'
+    to only affect the the enemies or 'bugs', and only do damage to them
+    */
+
+   
+}
