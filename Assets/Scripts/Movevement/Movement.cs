ced08bd39b38137cee1ec45fd777db2ab9604696
Basrah123 <elijah_weeks@stu.indianhills.edu>
2023-11-13 10:00:04 -0600

    adding sway

--- Assets/Scripts/Movevement/Movement.cs
+++ Assets/Scripts/Movevement/Movement.cs
@@ -1,37 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
-    [SerializeField] float movement_speed;
-    private CharacterController myController;
-    public float Gravity = 9.8f;
-    private float velocity = 0f;
-    private void Start()
-    {
-        myController= GetComponent<CharacterController>();
-    }
-    private void Update()
-    {
-        MoveMe();
-    }
+// public class Movement : MonoBehaviour
+// {
+//     [SerializeField] float movement_speed;
+//     private CharacterController myController;
+//     public float Gravity = 9.8f;
+//     private float velocity = 0f;
+//     private void Start()
+//     {
+//         myController= GetComponent<CharacterController>();
+//     }
+//     private void Update()
+//     {
+//         MoveMe();
+//     }
 
-    private void MoveMe()
-    {
-        float horizontal = Input.GetAxis("Horizontal") * movement_speed;
-        float vertical = Input.GetAxis("Vertical") * movement_speed;
-        myController.Move((Vector3.right * horizontal + Vector3.forward * vertical) * Time.deltaTime);
+//     private void MoveMe()
+//     {
+//         float horizontal = Input.GetAxis("Horizontal") * movement_speed;
+//         float vertical = Input.GetAxis("Vertical") * movement_speed;
+//         myController.Move((Vector3.right * horizontal + Vector3.forward * vertical) * Time.deltaTime);
 
-        // Gravity
-        if (myController.isGrounded)
-        {
-            velocity = 0;
-        }
-        else
-        {
-            velocity -= Gravity * Time.deltaTime;
-            myController.Move(new Vector3(0, velocity, 0));
-        }
-    }
-}
+//         // Gravity
+//         if (myController.isGrounded)
+//         {
+//             velocity = 0;
+//         }
+//         else
+//         {
+//             velocity -= Gravity * Time.deltaTime;
+//             myController.Move(new Vector3(0, velocity, 0));
+//         }
+//     }
+// }
--- Assets/Scripts/Shooting/KeyBoard.cs
+++ Assets/Scripts/Shooting/KeyBoard.cs
@@ -1,56 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEngine;
-public class KeyBoard : MonoBehaviour
-{
-    // public variables 
-    public Transform bulletSpawnPoint; 
-    public GameObject bulletPrefab; 
-    public float bulletSpeed = 50; 
-    // variables for relaoding 
-    public int currentAmmo, maxClipSize = 24; 
+// using System.Collections;
+// using System.Collections.Generic;
+// using Unity.VisualScripting;
+// using UnityEngine;
+// public class KeyBoard : MonoBehaviour
+// {
+//     // public variables 
+//     public Transform bulletSpawnPoint; 
+//     public GameObject bulletPrefab; 
+//     public float bulletSpeed = 50; 
+//     // variables for relaoding 
+//     public int currentAmmo, maxClipSize = 24; 
     
-    // logic for reloading 
-            // check for ammo 
-            // if current ammmo not 0 then shoot 
-            // while shoot (space), subtract from current ammo
-            // if current ammo = 0, cannot shoot 
+//     // logic for reloading 
+//             // check for ammo 
+//             // if current ammmo not 0 then shoot 
+//             // while shoot (space), subtract from current ammo
+//             // if current ammo = 0, cannot shoot 
 
-    void Start() 
-    {
-        // Initialize the ammo clip in the keyboard 
-        currentAmmo = maxClipSize; 
-    }
+//     void Start() 
+//     {
+//         // Initialize the ammo clip in the keyboard 
+//         currentAmmo = maxClipSize; 
+//     }
 
-       void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (currentAmmo > 0)
-            {
-                // You have ammo, so you can shoot.
-                var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-                bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
-                // Subtract one from the current ammo amount.
-                currentAmmo--;
-            }
-            else
-            {
-                // right now the user cannot shoot if current ammo is 0, 
-                // can implement additional logic
-                // for when the user runs out, message or sound 
-            }
-        }
-        // logic to allow for reloading the keyboard
-        if (Input.GetKeyDown (KeyCode.R)){
-            Reload(); 
-        }
-    }
-    // method for reloading, set the current ammo amt to the max clip 
-    void Reload()
-    {
-        // Refill the current ammo amount to the max clip size 
-        currentAmmo = maxClipSize; 
-    }
-}
+//        void Update()
+//     {
+//         if (Input.GetKeyDown(KeyCode.Space))
+//         {
+//             if (currentAmmo > 0)
+//             {
+//                 // You have ammo, so you can shoot.
+//                 var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+//                 bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+//                 // Subtract one from the current ammo amount.
+//                 currentAmmo--;
+//             }
+//             else
+//             {
+//                 // right now the user cannot shoot if current ammo is 0, 
+//                 // can implement additional logic
+//                 // for when the user runs out, message or sound 
+//             }
+//         }
+//         // logic to allow for reloading the keyboard
+//         if (Input.GetKeyDown (KeyCode.R)){
+//             Reload(); 
+//         }
+//     }
+//     // method for reloading, set the current ammo amt to the max clip 
+//     void Reload()
+//     {
+//         // Refill the current ammo amount to the max clip size 
+//         currentAmmo = maxClipSize; 
+//     }
+// }
